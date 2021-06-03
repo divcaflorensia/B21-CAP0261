3cc98ebf3f798d6030bce9d9a3a16508e7e4ac04
divcaflorensia <83702565+divcaflorensia@users.noreply.github.com>
2021-06-03 09:15:09 +0700

    Update predict_v2.py

--- colab_master/predict_v2.py
+++ colab_master/predict_v2.py
@@ -14,7 +14,23 @@ ran = np.random.random_sample(input_shape)
 input_data = np.array(ran, dtype=np.float32)
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
+from tensorflow.keras import layers,Sequential
+from tensorflow.keras.models import Model
+# Adding custom layers
+x = base_model.output
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dropout(0.2)(x)
+x = layers.Dense(1024, activation="relu")(x)
+predictions = layers.Dense(num_class, activation="softmax")(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
 interpreter.invoke()
+
+# Compile the model
+print('Compiling Model')
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy']
 
 # The function `get_tensor()` returns a copy of the tensor data.
 # Use `tensor()` in order to get a pointer to the tensor.
